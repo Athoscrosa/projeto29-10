d055a6c3e6049d1302f16aa624b70d1803cfa316
Athos Cordeiro Rosa <athoscordeiro@hotmail.com>
2025-11-05 20:52:25 -0500

    commit

--- html/js/user.js
+++ html/js/user.js
@@ -1,3 +1,13 @@
-$('$cpf'). inputmask(
-    
-)
+import { Validate } from "./Validate.js";
+
+const InsertButton = document.getElementById('insert');
+
+$('#cpf').inputmask({"mask": ["999.999.999-99", "99.999.999/9999-99"] });
+
+$('$celular').inputmask({"mask": ["(69) 99999-9999"] });
+
+InsertButton.addEventListener('click', async () => {
+    const IsValid = Validate
+        .SetId('form')
+        .Validate();
+});
